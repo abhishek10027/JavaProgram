e927915ccfa4d64cb8dcc904f89ab02bbf1ad5ae
Abhishek Kushwaha <ak846675@gmail.com>
2023-05-04 21:07:52 +0530

    first commit

---
+++ JavaProgram/sum.java
@@ -0,0 +1,16 @@
+package JavaProgram;
+
+import java.util.Scanner;
+
+public class sum {
+	public static void main(String args[]) {
+		System.out.println("Enter your first number: ");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println("Enter your second  number: ");
+		int b=sc.nextInt();
+		System.out.println("Addition of"+a+"+"+b+" number: "+ (a+b));
+		
+	}
+
+}
